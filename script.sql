8c218d1aa44697fb54539e11ecbb133a224f9e4d
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-02-06 19:16:03 +0000

    insert data

--- script.sql
+++ script.sql
@@ -98,3 +98,39 @@ unique (genre);
 alter table "member"  
 add constraint unique_id_passport
 unique (id_passport);
+
+
+
+insert into member (id_passport, "name", surname, birthday, phone)
+select distinct tv.dni, tv.nombre, concat(apellido_1, ' ', apellido_2) apellidos, cast(tv.fecha_nacimiento as date), tv.telefono 
+from tmp_videoclub tv ;
+
+insert into address (id_member, street, number, extension, postal_code)
+select distinct m.id_member, tv.calle, tv.numero, concat(piso, ' ', letra) exten, cast(tv.codigo_postal as INT)
+from tmp_videoclub tv 
+inner join "member" m on id_passport = tv.dni;
+
+insert into director (director)
+select distinct director 
+from tmp_videoclub tv ;
+
+insert into genre (genre)
+select distinct genero 
+from tmp_videoclub tv ;
+
+insert into film (title, sinopsis, year, id_genre, id_director)
+select distinct tv.titulo, tv.sinopsis, tv.anyo, g.id_genre, d.id_director 
+from tmp_videoclub tv 
+inner join genre g on genre = tv.genero
+inner join director d on d.director = tv.director
+;
+
+insert into stock (id_stock, id_film)
+select distinct tv.id_copia, f.id_film 
+from tmp_videoclub tv
+inner join film f on title = tv.titulo;
+
+insert into rental (pickup, return, id_stock, id_member)
+select cast(tv.fecha_alquiler as date), cast(tv.fecha_devolucion as date), tv.id_copia, m.id_member  
+from tmp_videoclub tv
+inner join "member" m on id_passport = tv.dni;
