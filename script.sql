0839f27815333a7c69d50ac3f13aa1bbff98ae61
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-02-07 18:48:08 +0000

    query2 favourite genres

--- script.sql
+++ script.sql
@@ -149,3 +149,43 @@ where s.id_stock not in (
 	where "return" is null
 	)
 ;
+
+-- QUERY2: Check members favourite genre 
+
+-- This option returns only one favourite genre, even member has the same amount of films consumed from two different genre
+
+select id_member, max(concat(num_genre, '-', genre)) as favourite
+from(
+	select r.id_member, count(g.genre) as num_genre, g.genre
+	from rental r
+	inner join stock s on s.id_stock = r.id_stock 
+	inner join film f on f.id_film = s.id_film 
+	inner join genre g on g.id_genre = f.id_genre 
+	group by r.id_member, g.genre
+	) as sub1
+group by id_member
+;
+
+-- This option returns genres rented by a member with the same maximum rental count per genre, thus it can be more than one favourite genre per member
+
+select sub1.id_member, concat(num_genre, '-', genre) as favourite
+from (
+    select r.id_member, count(g.genre) as num_genre, g.genre
+	from rental r
+	inner join stock s on s.id_stock = r.id_stock 
+	inner join film f on f.id_film = s.id_film 
+	inner join genre g on g.id_genre = f.id_genre 
+	group by r.id_member, g.genre
+	) as sub1
+	inner join(
+	    select id_member, max(num_genre) as MAXnum_genre
+	    from (
+	        select r.id_member, count(g.genre) as num_genre
+	        from rental r
+	        inner join stock s on s.id_stock = r.id_stock 
+			inner join film f on f.id_film = s.id_film 
+			inner join genre g on g.id_genre = f.id_genre 
+			group by r.id_member, g.genre
+	    ) as sub2
+    group by sub2.id_member
+) as sub on sub1.id_member = sub.id_member and sub1.num_genre = sub.MAXnum_genre;
